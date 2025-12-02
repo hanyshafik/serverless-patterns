3a51c1ce66fddf04c932e2caccb68bae8ba4de27
Hany Mostafa <hany.mkhalil@gmail.com>
2025-12-02 14:35:51 -0800

    Change Contact to GlucoseReadings

--- msk-lambda-schema-avro-java-sam/kafka_event_consumer_function/src/main/java/com/amazonaws/services/lambda/samples/events/kafka/AvroKafkaHandler.java
+++ msk-lambda-schema-avro-java-sam/kafka_event_consumer_function/src/main/java/com/amazonaws/services/lambda/samples/events/kafka/AvroKafkaHandler.java
@@ -10,27 +10,27 @@ import software.amazon.lambda.powertools.kafka.Deserialization;
 import software.amazon.lambda.powertools.kafka.DeserializationType;
 import software.amazon.lambda.powertools.logging.Logging;
 
-public class AvroKafkaHandler implements RequestHandler<ConsumerRecords<String, Contact>, String> {
+public class AvroKafkaHandler implements RequestHandler<ConsumerRecords<String, GlucoseReading>, String> {
     private static final Logger LOGGER = LoggerFactory.getLogger(AvroKafkaHandler.class);
 
     @Override
     @Logging(logEvent = true)
     @Deserialization(type = DeserializationType.KAFKA_AVRO)
-    public String handleRequest(ConsumerRecords<String, Contact> records, Context context) {
+    public String handleRequest(ConsumerRecords<String, GlucoseReading> records, Context context) {
         LOGGER.info("=== AvroKafkaHandler called ===");
         LOGGER.info("Event object: {}", records);
         LOGGER.info("Number of records: {}", records.count());
         
-        for (ConsumerRecord<String, Contact> record : records) {
+        for (ConsumerRecord<String, GlucoseReading> record : records) {
             LOGGER.info("Processing record - Topic: {}, Partition: {}, Offset: {}", 
                        record.topic(), record.partition(), record.offset());
             LOGGER.info("Record key: {}", record.key());
             LOGGER.info("Record value: {}", record.value());
             
             if (record.value() != null) {
-                Contact contact = record.value();
-                LOGGER.info("Contact details - firstName: {}, zip: {}", 
-                           contact.getFirstname(), contact.getZip());
+                GlucoseReading glucoseReading = record.value();
+                LOGGER.info("GlucoseReading details - patientId: {}, glucoseMgDL: {}", 
+                          glucoseReading.getPatientId(), glucoseReading.getGlucoseMgDL());
             }
         }
         
